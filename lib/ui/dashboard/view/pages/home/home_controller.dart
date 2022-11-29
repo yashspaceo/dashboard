a34151d42181a1476f5f2c27311b8f033286dd88
sotsys343 <yashma.spaceo@gmail.com>
2022-11-29 13:44:11 +0530

    third commit

--- lib/ui/dashboard/view/pages/home/home_controller.dart
+++ lib/ui/dashboard/view/pages/home/home_controller.dart
@@ -1,9 +1,9 @@
-// import 'dart:convert';
+import 'dart:convert';
 import 'package:dashboard/ui/dashboard/view/pages/home/model/product_model.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
-// import 'package:http/http.dart' as http;
+import 'package:http/http.dart' as http;
 
 class HomeController extends GetxController {
   RxList<Product> productList = <Product>[].obs;
@@ -12,26 +12,26 @@ class HomeController extends GetxController {
   @override
   void onInit() {
     super.onInit();
-    // fetchProduct();
+    fetchProduct();
     getToken();
   }
 
   /// Fetch Product data from api
-  // void fetchProduct() async {
-  //   isLoading.value = true;
-  //   final response = await http.get(Uri.parse('https://fakestoreapi.com/products'));
-  //   isLoading.value = false;
-  //
-  //   if (response.statusCode == 200) {
-  //     List<Product> list =
-  //         (jsonDecode(response.body) as List<dynamic>).map<Product>((value) => Product.fromJson(value)).toList();
-  //     productList.assignAll(list);
-  //   } else {
-  //     throw Exception(
-  //       'Failed to load Product data..',
-  //     );
-  //   }
-  // }
+  void fetchProduct() async {
+    isLoading.value = true;
+    final response = await http.get(Uri.parse('https://fakestoreapi.com/products'));
+    isLoading.value = false;
+
+    if (response.statusCode == 200) {
+      List<Product> list =
+          (jsonDecode(response.body) as List<dynamic>).map<Product>((value) => Product.fromJson(value)).toList();
+      productList.assignAll(list);
+    } else {
+      throw Exception(
+        'Failed to load Product data..',
+      );
+    }
+  }
 
   Future<String> getToken() async {
     String? token = await FirebaseMessaging.instance.getToken();
